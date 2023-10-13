9f86149a184c1ad1913313650e7a34a13e661672
David Castillo <casvel.d@gmail.com>
2023-10-12 20:15:42 -0700

    Add support for JSON response and query params

---
+++ DotNetCoreSqlDb/Controllers/Constraints/ContainsHeader.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace DotNetCoreSqlDb.Controllers.Constraints
+{
+    public class ContainsHeader : Attribute, IActionConstraint
+    {
+        private readonly string _value;
+        private readonly string _header;
+
+        public ContainsHeader(string header, string value)
+        {
+            _header = header;
+            _value = value;
+        }
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            if (!context.RouteContext.HttpContext.Request.Headers.ContainsKey(_header))
+            {
+                return false;
+            }
+
+            var headerValue = context.RouteContext.HttpContext.Request.Headers[_header];
+            return headerValue.Contains(_value);
+        }
+
+        public int Order => 0;
+    }
+}
